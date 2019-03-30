a13d120443afcd56305a43df4b6a36795dcb8f5d
LuanaMora <luanamora32@gmail.com>
2019-03-29 22:04:51 -0300

    Adicionado Angular Material no projeto

--- CalculaFrete/src/app/app.module.ts
+++ CalculaFrete/src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './cadastros/erros/page-not-found/page-not-found.component';
 import { FreteComponent } from './cadastros/frete/frete.component';
 
+import { MatInputModule, MatRadioModule, MatButtonModule, 
+  MatTableModule, 
+  MatPaginatorModule,
+  MatSortModule} from '@angular/material';
+
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +23,17 @@ import { FreteComponent } from './cadastros/frete/frete.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule, 
+    MatTableModule,
+    MatIconModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
