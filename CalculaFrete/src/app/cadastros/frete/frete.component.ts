d29cd995e3958d1889a7f0fd4f295f72dabcf1d7
Eduardo Zils <eduardo.m.zils@gmail.com>
2019-03-29 22:19:32 -0300

    Modificações da aula 29/03

--- CalculaFrete/src/app/cadastros/frete/frete.component.ts
+++ CalculaFrete/src/app/cadastros/frete/frete.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FreteComponent implements OnInit {
 
+  displayedCollumns: string[] = ['freteId'];
+
   constructor() { }
 
   ngOnInit() {
