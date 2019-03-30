b4410749c726ab303eb944bc3248a6ba6a6e4cf6
LuanaMora <luanamora32@gmail.com>
2019-03-29 21:26:52 -0300

    Commit Classe cep, cidade, estado

---
+++ CalculaFrete/src/app/cadastros/models/cep.ts
@@ -0,0 +1,3 @@
+export class Cep{
+    public cep: string;
+}
---
+++ CalculaFrete/src/app/cadastros/models/cidade.ts
@@ -0,0 +1,8 @@
+import { Cep } from "./cep";
+
+export class Cidade {
+    public codigo: number;
+    public descricao: string;
+    public cepList: Array<Cep>;
+}
+
