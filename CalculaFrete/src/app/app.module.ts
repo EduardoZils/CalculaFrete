2182511464a1102bedca197ee7b2329e9af25d94
Eduardo Zils <eduardo.m.zils@gmail.com>
2019-03-29 21:00:44 -0300

    CriacaoDeCadastros

--- CalculaFrete/src/app/app.module.ts
+++ CalculaFrete/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CepComponent } from './cadastros/cep/cep.component';
+import { EstadoComponent } from './cadastros/estado/estado.component';
+import { CidadeComponent } from './cadastros/cidade/cidade.component';
+import { PageNotFoundComponent } from './cadastros/erros/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CepComponent,
+    EstadoComponent,
+    CidadeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
