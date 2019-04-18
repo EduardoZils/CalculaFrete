77b4e054c33e44d02a0699a615a8da23cb9f754f
LuanaMora <luanamora32@gmail.com>
2019-04-18 18:37:48 -0300

    Alterações no ts e adicionado icones

--- CalculaFrete/src/app/cadastros/frete/frete.component.ts
+++ CalculaFrete/src/app/cadastros/frete/frete.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Estado} from '../models/estado'
+import { Estado } from '../models/estado'
 import { Cidade } from '../models/cidade';
 import { Cep } from '../models/cep';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
@@ -19,16 +19,16 @@ export class FreteComponent implements OnInit {
   public cidadeSel: Cidade = new Cidade;
   public cidadeModel: Cidade = new Cidade();
   public estadoModel: Estado = new Estado();
-  
-  public estados:  any;
-  public cidades:  any;
-  public ceps:  any;
+
+  public estados: any;
+  public cidades: any;
+  public ceps: any;
   public dataSource: any;
 
-  
-  displayColumnsEstado: string[] = ['codigo', 'uf', 'descricao' ];
+
+  displayColumnsEstado: string[] = ['actionsColumn', 'codigo', 'uf', 'descricao'];
   displayColumnsCidade: string[] = ['codigo', 'descricao'];
- 
+
 
   @ViewChild(MatPaginator) paginatorCustom: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -42,13 +42,17 @@ export class FreteComponent implements OnInit {
     this.isExpandido = 0;
 
     this.criaDados();
+    this.dataSource = new MatTableDataSource<Estado>(this.estados);
+    this.dataSource.paginator = this.paginatorCustom;
+    console.log("DATA SOURCE" + this.dataSource)
+    console.log(this.estados);
   }
 
   setExpandido() {
     this.isExpandido = 1;
   }
 
-  criaDados(){
+  criaDados() {
     let estado;
     let cidade;
     let cidades = new Array<Cidade>();
@@ -59,100 +63,104 @@ export class FreteComponent implements OnInit {
     cep = new Cep();
     cep.descricao = "123456-78";
     ceps.push(cep);
-    
+
     cep = new Cep();
     cep.descricao = "876543-21";
     ceps.push(cep);
 
     cidade = new Cidade();
     cidade.codigo = 1;
-    cidade.descricao = "Cidade 1";
+    cidade.descricao = "Cascavel";
     cidade.cepList = ceps;
     cidades.push(cidade);
-    
+
     cidade = new Cidade();
     cidade.codigo = 2;
-    cidade.descricao = "Cidade 2";
+    cidade.descricao = "Toledo";
     cidade.cepList = ceps;
     cidades.push(cidade);
+
+    estado = new Estado();
+    estado.codigo = 1;
+    estado.descricao = "Paraná";
+    estado.uf = "PR";
+    estado.cidadeList = cidades;
+    this.estados.push(estado);
+
+    estado = new Estado();
+    estado.codigo = 2;
+    estado.descricao = "Rio Grande do Sul";
+    estado.uf = "RG";
+    estado.cidadeList = cidades;
+    this.estados.push(estado);
+  }
+
+
+  atualizarEstadoListBox() {
+    this.dataSource = new Array<Estado>();
+    console.log("Chamou atualizarEstadoListBox codigo -------> " + this.estadoSelId);
+    let id = this.estadoSelId;
+    let estadoSelLocal;
+    this.estados.forEach(item => {
+      if (item.estadoId == id) {
+        estadoSelLocal = item;
+        alert("Propriedade da pessoa selecionada " + item.cidades)
+      }
+    });
+    this.estadoSel = estadoSelLocal;
+  }
+
+  atualizarCidadeListBox() {
+    console.log("Chamou atualizarEstadoListBox codigo -------> " + this.cidadeSelId);
+    let id = this.cidadeSelId;
+    let cidadeSelLocal;
+    this.estados.forEach(item => {
+      if (item.cidadeId == id) {
+        cidadeSelLocal = item;
+        alert("Propriedade da pessoa selecionada " + item.cidades)
+      }
+    });
+    this.cidadeSel = cidadeSelLocal;
+  }
+
+
+  salvarEstado() {
+    console.log("Estado Salvo");
+    console.log(this.estado)
+    this.estados.push(this.estado);
+    console.log("Lista de Estados");
+    console.log(this.estados);
+    this.estado = new Estado();
+    this.dataSource = new MatTableDataSource<Estado>(this.estados);
+    this.dataSource.paginator = this.paginatorCustom;
+    console.log("DATA SOURCE" + this.dataSource)
+    console.log(this.estados);
+  }
+
+
+  sortData() {
+    this.dataSource.sort = this.sort;
+  }
+
+  salvarCidade() {
+    this.cidades.push(this.cidadeModel);
+    console.log("Cidade salva");
+    console.log(this.cidades);
+  }
+
+  salvarCEP() {
+    let cep = new Cep();
+    let id = this.cidadeSelId
+    let cepsCidade = this.cidadeSel.cepList;
+    console.log("Cep Salvo -->" + cep);
+    this.cidades.forEach(item => {
+      if (item.cidadeID == id) {
+        item.cepList.push(cepsCidade);
+      }
+    });
+  }
+
+  voltar(){
     
-      estado = new Estado();
-      estado.codigo = 1;
-      estado.descricao = "Estado1";  
-      estado.uf = "E1";
-      estado.cidadeList = cidades;
-      this.estados.push(estado);
-
-      estado = new Estado();
-      estado.codigo = 2;
-      estado.descricao = "Estado2";  
-      estado.uf = "E2";
-      estado.cidadeList = cidades;
-      this.estados.push(estado);
-    }
-  
-
-    atualizarEstadoListBox(){
-      this.dataSource = new Array<Estado>();
-      console.log("Chamou atualizarEstadoListBox codigo -------> " + this.estadoSelId);
-      let id = this.estadoSelId;
-      let estadoSelLocal;
-      this.estados.forEach(item => {
-        if(item.estadoId == id){
-          estadoSelLocal = item;
-          alert("Propriedade da pessoa selecionada "+ item.cidades)
-        }
-      });
-      this.estadoSel = estadoSelLocal;
-    }
-    
-    atualizarCidadeListBox(){
-      console.log("Chamou atualizarEstadoListBox codigo -------> " + this.cidadeSelId);
-      let id = this.cidadeSelId;
-      let cidadeSelLocal;
-      this.estados.forEach(item => {
-        if(item.cidadeId == id){
-          cidadeSelLocal = item;
-          alert("Propriedade da pessoa selecionada "+ item.cidades)
-        }
-      });
-      this.cidadeSel = cidadeSelLocal;
-    }
-   
-
-    salvarEstado(){
-      console.log("Estado Salvo");
-      console.log(this.estado)
-      this.estados.push(this.estado);
-      console.log("Lista de Estados");
-      console.log(this.estados);
-      this.estado = new Estado();
-      this.dataSource = new MatTableDataSource<Estado>(this.estados);
-
-      this.dataSource.paginator = this.paginatorCustom; 
-      console.log("DATA SOURCE" + this.dataSource)
-      console.log(this.estados);
-    }
-
-    sortData(){
-      this.dataSource.sort = this.sort;
-    }
-
-    salvarCidade(){
-      this.cidades.push(this.cidadeModel);
-      console.log("Cidade salva");
-      console.log(this.cidades);
-    }
-
-    salvarCEP(){
-      let cep = new Cep();
-      let id = this.cidadeSelId
-      let cepsCidade = this.cidadeSel.cepList;
-      console.log("Cep Salvo -->" + cep);
-      this.cidades.forEach(item => {
-        if(item.cidadeID == id){
-          item.cepList.push(cepsCidade);
-        }
-      });
-    }
+  }
 }
