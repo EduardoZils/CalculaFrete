c8d799f8152c2a22f898f4de9a0473a754de006d
Eduardo Zils <eduardo.m.zils@gmail.com>
2019-04-23 19:53:12 -0300

    Commit neutro

--- CalculaFrete/src/app/cadastros/models/cidade.ts
+++ CalculaFrete/src/app/cadastros/models/cidade.ts
@@ -1,9 +1,11 @@
 import { Cep } from "./cep";
+import { Estado } from "./estado";
 
 
 export class Cidade {
     public codigo: number;
     public descricao: string;
     public cepList: Array<Cep>;
+    public estado: Estado;
 }
 
